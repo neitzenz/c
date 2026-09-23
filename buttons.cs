8cfb34da4931df3636469d4c911e7e190868f894
neitzenz <radislavakupov131@gmail.com>
2026-09-23 06:08:34 +0500

    Add files via upload

---
+++ buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/hikarii-dev/veloria-cs2-dumper
+// 2026-09-23 00:15:28.259476800 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x222B040;
+        public const nint attack2 = 0x222B0D0;
+        public const nint back = 0x222B310;
+        public const nint duck = 0x222B5E0;
+        public const nint forward = 0x222B280;
+        public const nint jump = 0x222B550;
+        public const nint left = 0x222B3A0;
+        public const nint lookatweapon = 0x25709A0;
+        public const nint reload = 0x222AFB0;
+        public const nint right = 0x222B430;
+        public const nint showscores = 0x2570880;
+        public const nint sprint = 0x222AF20;
+        public const nint turnleft = 0x222B160;
+        public const nint turnright = 0x222B1F0;
+        public const nint use = 0x222B4C0;
+        public const nint zoom = 0x2570910;
+    }
+}
